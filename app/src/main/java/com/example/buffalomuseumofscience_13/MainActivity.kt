399a6d8a5661adc98360792a6bfbefe43850ee7b
epicseven-cup <jllin3@buffalo.edu>
2023-07-06 13:39:27 -0400

    frontpage

--- app/src/main/java/com/example/buffalomuseumofscience_13/MainActivity.kt
+++ app/src/main/java/com/example/buffalomuseumofscience_13/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.buffalomuseumofscience_13
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Setting the login the button to the onclick event
+//        findViewById<Button>(R.id.login_button).setOnClickListener{
+//
+//        }
     }
 }
---
+++ app/src/main/java/com/example/buffalomuseumofscience_13/loginButton.kt
@@ -0,0 +1,9 @@
+package com.example.buffalomuseumofscience_13
+
+import android.view.View
+
+class loginButton: View.OnClickListener {
+    override fun onClick(p0: View?) {
+        println("Login button is clicked")
+    }
+}
